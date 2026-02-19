9fc0b2e5b26b66bd9f2cc9bc2a37db29ef4eb704
alxvkn <64454213+alxvkn@users.noreply.github.com>
2026-02-20 00:55:44 +0300

    fix argc check

--- src/ui.c
+++ src/ui.c
@@ -6,7 +6,7 @@
 #define EXITERR(msg, code) do { puts(msg); exit(code); } while(0)
 
 int ui(int argc, char* argv[], double (*calc)(double)) {
-    if (argc == 0) EXITERR("no arguments", -1);
+    if (argc == 1) EXITERR("no arguments", -1);
 
     double input = 0;
     sscanf(argv[1], "%lf", &input);
